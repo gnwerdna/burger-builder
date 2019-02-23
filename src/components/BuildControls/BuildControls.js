647fcdb415904aefcc7443b88a61c52f2dbbf874
gnwerdna <gnwerdna.cs@gmail.com>
2019-02-23 15:44:59 +0700

    outputting multipe build control

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import BuildControl from '../BuildControls/BuildControl/BuildControl';
+import classes from './BuildControls.module.css';
+const controls = [
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Salad', type: 'salad' },
+];
 
 const buildControls = (props) => {
     return (
-        <div></div>
+        <div className={classes.BuildControls}>
+            {
+                controls.map(ctrl => {
+                    return <BuildControl key={ctrl.label} label={ctrl.label} />
+                })
+            }
+        </div>
     )
 }
 
