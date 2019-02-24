a72f29964df6f9b1a4213b16f69422742c441b29
gnwerdna <gnwerdna.cs@gmail.com>
2019-02-24 17:10:22 +0700

    responsive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 const layout = (props) => {
     return (
         <Aux>
             <Toolbar />
+            <SideDrawer/>
             <main className={classes.Content}>
                 {props.children}
             </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+
+
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
