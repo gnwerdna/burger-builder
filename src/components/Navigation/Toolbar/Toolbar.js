c641a8f0d4645b286912e5ca2eb27a7c1460193e
gnwerdna <gnwerdna.cs@gmail.com>
2019-02-24 22:51:46 +0700

    responsive side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,14 @@ import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
-
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
-            <NavigationItems/>
+            <NavigationItems />
         </nav>
     </header>
 )
