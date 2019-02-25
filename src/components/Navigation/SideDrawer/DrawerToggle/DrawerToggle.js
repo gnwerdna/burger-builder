fd9d9d9e92ea247c0f4192ed8f459e2b6dc468a5
gnwerdna <gnwerdna.cs@gmail.com>
2019-02-25 15:04:22 +0700

    change menu button

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import classes from './DrawerToggle.module.css';
 
 const drawerToggle = (props) => (
-    <div onClick={props.toggleClicked}>Menu</div>
+    <div onClick={props.toggleClicked} className={classes.DrawerToggle}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 )
 
 export default drawerToggle;
