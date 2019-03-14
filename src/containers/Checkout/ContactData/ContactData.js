bb16cfaea0ddaa17c2ce30798ee8307ef9e0eeb8
gnwerdna <gnwerdna.cs@gmail.com>
2019-03-14 17:11:19 +0700

    adding custom form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -13,7 +13,11 @@ class ContactData extends React.Component {
                     type: 'text',
                     placeholder: 'Your name'
                 },
-                value: 'Dai D. Nguyen'
+                value: 'Dai D. Nguyen',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -21,7 +25,11 @@ class ContactData extends React.Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: 'input',
@@ -29,7 +37,13 @@ class ContactData extends React.Component {
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -37,7 +51,11 @@ class ContactData extends React.Component {
                     type: 'email',
                     placeholder: 'Your email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -80,6 +98,22 @@ class ContactData extends React.Component {
             });
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (rules.required) {
+            isValid = value.trim() !== "" && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.minLength >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.maxLength <= rules.maxLength && isValid;
+        }
+        return isValid;
+    }
+
     inputChangeHandler = (event, inputIdentify) => {
         // console.log(event.target.value);
         
@@ -90,8 +124,8 @@ class ContactData extends React.Component {
         const updateFormElement = {
             ...updateOrderForm[inputIdentify]
         }
-
         updateFormElement.value = event.target.value;
+        updateFormElement.valid = this.checkValidity(updateFormElement.value, updateFormElement.validation)
         updateOrderForm[inputIdentify] = updateFormElement;
         this.setState({orderForm: updateOrderForm});
     }
