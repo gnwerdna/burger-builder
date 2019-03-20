04f90f977af7b35ed33f712b04f8f1b49f57ad14
gnwerdna <gnwerdna.cs@gmail.com>
2019-03-20 16:24:26 +0700

    redirect to improve ux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import ContactData from '../../containers/Checkout/ContactData/ContactData';
 import { connect } from 'react-redux'
 class Checkout extends React.Component {
@@ -13,17 +13,23 @@ class Checkout extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-                <Route
-                    path={this.props.match.path + "/contact-data"}
-                    component={ContactData}
+        let summary = <Redirect to="/" />;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler} />
+                    <Route
+                        path={this.props.match.path + "/contact-data"}
+                        component={ContactData}
                     />
-            </div>
+                </div>
+            );
+        }
+        return (
+            { summary }
         );
     }
 }
