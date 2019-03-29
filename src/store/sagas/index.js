b2311d94a00407b2970ff2a19638b5e40b3cd868
gnwerdna <gnwerdna.cs@gmail.com>
2019-03-29 15:31:19 +0700

    deep dive saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, all } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes'
 import {
     logoutSaga,
@@ -9,10 +9,16 @@ import {
 import { initIngredientSaga } from './burgerBuilder';
 import { purchaseBurgerSaga, fetchOrdersSaga } from './order'
 export function* watchAuth() {
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+    // yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    // yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+    // yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+    // yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+    yield all([
+        takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    ]);
 }
 
 export function* watchBurgerBuilder() {
